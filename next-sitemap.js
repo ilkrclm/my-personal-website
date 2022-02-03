16293c9e3f28fd2fe76be8f80ce6561bdef919d7
ilkrclm <ilkercalim@hotmail.com>
2022-02-03 23:46:06 +0300

    sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,33 +1,14 @@
-const siteUrl = process.env.NEXT_PUBLIC_URL;
+const site = `${process.env.NEXT_PUBLIC_URL}`
 
 module.exports = {
-  siteUrl,
-  generateRobotsTxt: true,
+  siteUrl: `${site}`,
   exclude: ['/tr/*', '/tr'],
-  sitemapSize: 1000,
-  changefreq: 'daily',
   priority: 1.0,
   alternateRefs: [
-  {
-    href: `${siteUrl}/en`,
-    hreflang: 'en',
+    {
+      href: `${site}/en`,
+      hreflang: 'en',
     },
   ],
-  transform: async (config, path) => {
-    return {
-      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: config.alternateRefs ?? [],
-    }
-  },
-  robotsTxtOptions: {
-    policies: [
-       {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-  }
-};
+  generateRobotsTxt: true,
+}
